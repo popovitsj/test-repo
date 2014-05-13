812013564c648e804b43f9a247efe3344918505e
Wim <popovitsj@gmail.com>
2014-05-14 00:30:18 +0200

    Nice 1

--- src/com/example/testinggit/MainActivity.java
+++ src/com/example/testinggit/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Bla
         setContentView(R.layout.activity_main);
     }
 
